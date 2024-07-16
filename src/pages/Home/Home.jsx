49dc45c739e3f2d88ee40b63a83cd37bccb58b16
BarnoMamadzhanova <b@mamadzhanova.com>
2024-07-16 13:50:14 +0200

    developing hpme page and initial styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import classes from "./Home.module.css";
+import CardGrid from "../../components/CardGrid/CardGrid";
 
 function Home() {
   return (
     <div className={classes.home}>
-      <h3>Home</h3>
+      <h6 className={classes.user_welcome}>Hi, Sarthak. UI Designer & Cook</h6>
+      <div className={classes.category_container}>
+        <h6 className={classes.category}>Category</h6>
+        <div className={classes.display_box}>
+          <ul className={classes.categories}>
+            <li>Breakfast</li>
+            <li>Lunch</li>
+            <li>Dinner</li>
+          </ul>
+          <CardGrid />
+        </div>
+      </div>
     </div>
   );
 }
