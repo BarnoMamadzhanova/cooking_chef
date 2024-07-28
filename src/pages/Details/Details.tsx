9edd8e3478da6f39e580d7fa7ffc24188c3bd5b0
BarnoMamadzhanova <b@mamadzhanova.com>
2024-07-28 14:28:42 +0200

    fixing the style of like and save icons

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -9,8 +9,30 @@ import {
   selectRecipeDetailLoading,
   selectRecipeDetailError,
 } from "../../redux/recipes/recipeSlice";
-import { time, like, save, back } from "../../assests/index";
+import { time, back } from "../../assests/index";
 import classes from "./Details.module.css";
+
+const likeIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+  >
+    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+  </svg>
+);
+
+const saveIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+  >
+    <path d="M19 21l-7-5-7 5V5c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2v16z" />
+  </svg>
+);
 
 const Details: React.FC = () => {
   const { recipeId } = useParams<{ recipeId: string }>();
@@ -93,20 +115,22 @@ const Details: React.FC = () => {
           <p>{recipeDetail.difficulty}</p>
         </div>
         <div className={classes.icon_box}>
-          <div className={classes.icons} onClick={handleLike}>
-            <img
-              src={like}
-              alt="like"
-              className={recipeDetail.isLikedByUser ? classes.liked : ""}
-            />
+          <div
+            className={`${classes.icons} ${
+              recipeDetail.isLikedByUser ? classes.liked : ""
+            }`}
+            onClick={handleLike}
+          >
+            {likeIcon}
             <p>{recipeDetail.likesAmount} likes</p>
           </div>
-          <div className={classes.icons} onClick={handleSave}>
-            <img
-              src={save}
-              alt="save"
-              className={recipeDetail.isSavedByUser ? classes.saved : ""}
-            />
+          <div
+            className={`${classes.icons} ${
+              recipeDetail.isSavedByUser ? classes.saved : ""
+            }`}
+            onClick={handleSave}
+          >
+            {saveIcon}
             <p>{recipeDetail.savesAmount} saves</p>
           </div>
         </div>
